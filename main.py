0aca939a7e130e685c56e5ee05e9d7ebce65e6c8
Sam Shams <samshams11206@gmail.com>
2026-06-14 19:30:12 -0400

    added imports

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from tkinter import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import KNeighborsClassifier
 
 
-
-def calculation():
+def prediction():
   pass
   pass
