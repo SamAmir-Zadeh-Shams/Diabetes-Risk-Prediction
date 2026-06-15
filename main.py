9fa70d4585065bfeb8b7177b4488e3cfe1560427
Sam Shams <samshams11206@gmail.com>
2026-06-15 03:25:09 -0400

    added code to train data

--- main.py
+++ main.py
@@ -5,8 +5,33 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report, confusion_matrix
 
+dataset = pd.read_csv("diabetes_prediction_dataset.csv")
+
+x = dataset.iloc[:, :-1]
+y = dataset.iloc[:,-1]
+
+dataset = pd.get_dummies(dataset) # get data and translate all data that is text into 1s and 0s
+
+y = dataset.pop('diabetes')
+x = dataset
+
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.20)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+classifier = KNeighborsClassifier(n_neighbors=5)
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+print(confusion_matrix(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 
 def prediction():
-  pass
   pass
